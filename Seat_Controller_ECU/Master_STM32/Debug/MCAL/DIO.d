c8c19524f7622a4e8a39c0db61eb93709e18c4eb
Moatasem <MoatasemElsharkawy12@gmail.com>
2025-09-10 01:23:44 +0300

    Edit on the 3 Folders of MCU'S

--- Seat_Controller_ECU/Master_STM32/Debug/MCAL/DIO.d
+++
@@ -1,8 +0,0 @@
-MCAL/DIO.o MCAL/DIO.o: ../MCAL/DIO.c ../MCAL/../LIB/BIT_MATH.h \
-  ../MCAL/../LIB/STD_TYPES.h ../MCAL/DIO.h
-
-../MCAL/../LIB/BIT_MATH.h:
-
-../MCAL/../LIB/STD_TYPES.h:
-
-../MCAL/DIO.h:
