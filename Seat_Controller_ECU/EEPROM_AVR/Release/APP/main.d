58e0e10823eb763e42f1e382fb0f0afbf9135c44
Amr Yasser <154547867+AMRYB@users.noreply.github.com>
2025-09-10 10:21:57 +0300

    The latest update from Moatassem

---
+++ Seat_Controller_ECU/EEPROM_AVR/Release/APP/main.d
@@ -0,0 +1,27 @@
+APP/main.o APP/main.o: ../APP/main.c ../APP/../MCAL/DIO/DIO.h \
+  ../APP/../MCAL/DIO/../../LIB/BIT_MATH.h \
+  ../APP/../MCAL/DIO/../../LIB/STD_TYPES.h ../APP/../LIB/BIT_MATH.h \
+  ../APP/../LIB/STD_TYPES.h ../APP/../MCAL/EEPROM/EEPROM.h \
+  ../APP/../MCAL/EEPROM/../../LIB/BIT_MATH.h \
+  ../APP/../MCAL/EEPROM/../../LIB/STD_TYPES.h \
+  ../APP/../MCAL/EEPROM/../../MCAL/DIO/DIO.h ../APP/../MCAL/UART/UART.h
+
+../APP/../MCAL/DIO/DIO.h:
+
+../APP/../MCAL/DIO/../../LIB/BIT_MATH.h:
+
+../APP/../MCAL/DIO/../../LIB/STD_TYPES.h:
+
+../APP/../LIB/BIT_MATH.h:
+
+../APP/../LIB/STD_TYPES.h:
+
+../APP/../MCAL/EEPROM/EEPROM.h:
+
+../APP/../MCAL/EEPROM/../../LIB/BIT_MATH.h:
+
+../APP/../MCAL/EEPROM/../../LIB/STD_TYPES.h:
+
+../APP/../MCAL/EEPROM/../../MCAL/DIO/DIO.h:
+
+../APP/../MCAL/UART/UART.h:
